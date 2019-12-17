b91313b54e9dccad515473ea8aae1c288b4da4ec
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-12-17 14:46:04 -0600

    set up queries for updating connections accept

--- server/routes/activity.router.js
+++ server/routes/activity.router.js
@@ -2,18 +2,47 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// ---- GET's ---- 
+// GET all connections for one user
 router.get('/', (req, res, next) => { 
-
   const queryText = `SELECT "connections"."id" AS "connections_id", "connecting_id", "connecting_to_id", "connector_id", "message", "connecting_accepted", "connecting_to_accepted", 
-  (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_avatar", 
-  (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_avatar", 
-  (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_avatar"
-  FROM "connections"
-  WHERE "connections"."connecting_id" = $1 OR "connections"."connecting_to_id" = $1 OR "connections"."connector_id" = $1;`;
+    (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_avatar", 
+    (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_avatar", 
+    (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_avatar"
+    FROM "connections"
+    WHERE "connections"."connecting_id" = $1 OR "connections"."connecting_to_id" = $1 OR "connections"."connector_id" = $1;`;
   pool.query(queryText, [req.user.id])
     .then((response) => res.send(response.rows))
-    .catch((error) => {console.log('Error in router GET User Activity', error)
-     res.sendStatus(500)});
+    .catch((error) => {
+      console.log('Error in router GET User Activity', error)
+      res.sendStatus(500)});
 });
+
+// ---- PUT's ----
+// UPDATE connecting_accepted to true
+router.put('/connecting-accept', (req, res) => {
+  const queryText = `UPDATE "connections"
+    SET "connecting_accepted" = true
+    WHERE "id" = $1;`;
+  pool.query(queryText, [req.body.connections_id])
+    .then(() => res.sendStatus(200))
+    .catch((error) => {
+      console.log('Error in router UPDATE connecting_accepted to true', error);
+      res.sendStatus(500)
+    })
+})
+// UPDATE connecting_to_accepted to true
+router.put('/connecting-to-accept', (req, res) => {
+  const queryText = `UPDATE "connections"
+    SET "connecting_to_accepted" = true
+    WHERE "id" = $1;`;
+  pool.query(queryText, [req.body.connections_id])
+    .then(() => res.sendStatus(200))
+    .catch((error) => {
+      console.log('Error in router UPDATE connecting_to_accepted to true', error);
+      res.sendStatus(500)
+    })
+})
+
 
 module.exports = router;
