c932e58100166934b0ed090bdbb3172812011a00
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-12-17 15:22:10 -0600

    updated query with new active column

--- server/routes/activity.router.js
+++ server/routes/activity.router.js
@@ -5,12 +5,12 @@ const router = express.Router();
 // ---- GET's ---- 
 // GET all connections for one user
 router.get('/', (req, res, next) => { 
-  const queryText = `SELECT "connections"."id" AS "connections_id", "connecting_id", "connecting_to_id", "connector_id", "message", "connecting_accepted", "connecting_to_accepted", 
+  const queryText = `SELECT "connections"."id" AS "connections_id", "connecting_id", "connecting_to_id", "connector_id", "message", "connecting_accepted", "connecting_to_accepted", "connections"."active",
     (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connector_id") AS "connector_avatar", 
     (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_id") AS "connecting_avatar", 
     (SELECT "user"."first_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_first_name", (SELECT "user"."last_name" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_last_name", (SELECT "user"."username" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_username", (SELECT "user"."avatar" FROM "user" WHERE "user"."id" = "connections"."connecting_to_id") AS "connecting_to_avatar"
     FROM "connections"
-    WHERE "connections"."connecting_id" = $1 OR "connections"."connecting_to_id" = $1 OR "connections"."connector_id" = $1;`;
+    WHERE ("connections"."connecting_id" = $1 OR "connections"."connecting_to_id" = $1 OR "connections"."connector_id" = $1) AND "connections"."active" = true;`;
   pool.query(queryText, [req.user.id])
     .then((response) => res.send(response.rows))
     .catch((error) => {
