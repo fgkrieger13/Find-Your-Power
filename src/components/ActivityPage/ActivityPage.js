b7a67a9285ad624b022d202c50a9bed27b13d2bb
Brooks Geenen <brgeen@gmail.com>
2019-12-18 16:12:40 -0600

    style user activity page

--- src/components/ActivityPage/ActivityPage.js
+++ src/components/ActivityPage/ActivityPage.js
@@ -2,135 +2,131 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ConnectionMade from '../ConnectionMade/ConnectionMade'
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
-
-
-
 
 class ActivityPage extends Component {
 
   componentDidMount() {
-
-
     this.props.dispatch({ type: 'FETCH_USER_ACTIVITY' })
   }
 
   changeConnectingAccepted = (id) => {
-    this.props.dispatch({ type: 'CHANGE_CONNECTING_ACCEPTED', payload: {connections_id: id}});
+    this.props.dispatch({ type: 'CHANGE_CONNECTING_ACCEPTED', payload: { connections_id: id } });
   }
 
   changeConnectingToAccepted = (id) => {
-    this.props.dispatch({ type: 'CHANGE_CONNECTING_TO_ACCEPTED', payload: {connections_id: id}});
+    this.props.dispatch({ type: 'CHANGE_CONNECTING_TO_ACCEPTED', payload: { connections_id: id } });
   }
 
   denyConnection = (id) => {
-    this.props.dispatch({ type: 'DENY_CONNECTION', payload: {connections_id: id}})
+    this.props.dispatch({ type: 'DENY_CONNECTION', payload: { connections_id: id } })
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="activity-page">
-          <p>
-            This is the ActivityPage
-          </p>
-          <div className="pending-connections">
-            <h3>Pending Connections</h3>
-            {(this.props.userActivity.length > 0) ?
-              (this.props.userActivity.filter(activity =>
-                ((activity.connecting_id === this.props.user.id || activity.connecting_to_id === this.props.user.id)
-                  && (!activity.connecting_accepted || !activity.connecting_to_accepted)))).map((activity) =>
-                    <div key={activity.connections_id}>
+      <div className="activity-container">
+        <div className="activity-pending-connections-container">
+          <div className="activity-title"><h1>Pending Connections</h1></div>
+          {(this.props.userActivity.length > 0) ?
+            (this.props.userActivity.filter(activity =>
+              ((activity.connecting_id === this.props.user.id || activity.connecting_to_id === this.props.user.id)
+                && (!activity.connecting_accepted || !activity.connecting_to_accepted)))).map((activity) =>
+                  <div key={activity.connections_id}>
+                    <div className="activity-pending-single-row">
+                      <div className= "activity-pending-leftmost">
                       <div className="avatar">
-                        This is where the image will go!
+                        <img className="activity-pending-avatar" src={activity.connecting_to_avatar} />
                       </div>
-                      <div>
-                          {activity.connecting_id === this.props.user.id ? 
-                            <h4>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</h4>
-                            :
-                            <h4>{activity.connecting_first_name} {activity.connecting_last_name}</h4>
-                          }
-                        <p>Suggested by:</p>
-                        <p>{activity.connector_first_name} {activity.connector_last_name}</p>
+                        {activity.connecting_id === this.props.user.id ?
+                          <div className= "activity-pending-title"> <h3>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</h3> </div>
+                          : 
+                          <h3>{activity.connecting_first_name} {activity.connecting_last_name}</h3>
+                        }
+              
+                        </div>
+                        <div className="activity-suggested-by"><h3>Suggested by:</h3></div>
+                        <div className= "activity-pending-suggestor">
+                        <div className= "activity-pending-connecting-avatar">
+                        <img className="activity-pending-avatar" src={activity.connector_avatar} />
+                        <h3>{activity.connector_first_name} {activity.connector_last_name}</h3>
+                        </div>
                       </div>
-                      <div>
-                        <p>Message:</p>
-                        <p>{activity.message}</p>
-                      </div>
-                      <div>
-                        {( activity.connecting_id === this.props.user.id && !activity.connecting_accepted) || (activity.connecting_to_id === this.props.user.id && !activity.connecting_to_accepted) ? 
-                        <div>
-                          <button onClick={
-                            activity.connecting_id === this.props.user.id ? 
-                              () => this.changeConnectingAccepted(activity.connections_id) 
-                              : 
-                              () => this.changeConnectingToAccepted(activity.connections_id) 
+                    
+                    <div className="activity-pending-message">
+                      <h3>Message:</h3>
+                      <p>{activity.message}</p>
+                    </div>
+                    <div>
+                      {(activity.connecting_id === this.props.user.id && !activity.connecting_accepted) || (activity.connecting_to_id === this.props.user.id && !activity.connecting_to_accepted) ?
+                        <div className="activity-pending-button-container">
+                          <button className="activity-pending-accept-button" onClick={
+                            activity.connecting_id === this.props.user.id ?
+                              () => this.changeConnectingAccepted(activity.connections_id)
+                              :
+                              () => this.changeConnectingToAccepted(activity.connections_id)
                           }>
                             Accept
                           </button>
-                          <button onClick={ () => this.denyConnection(activity.connections_id)}>
+                          <button className="activity-pending-cancel-button" onClick={() => this.denyConnection(activity.connections_id)}>
                             Deny
-                          </button> 
+                          </button>
+                        
                         </div>
+                        
                         :
                         ''
                       }
- 
-                      </div>
                     </div>
-                  )
-              : ''
-            }
-          </div>
-          <div className="people-who-connected-you">
-            <h3>People Who Connected You</h3>
-            {(this.props.userActivity.length > 0) ?
-              <table>
-                <tbody>
-                  {(this.props.userActivity.filter(activity =>
-                    ((activity.connecting_id === this.props.user.id || activity.connecting_to_id === this.props.user.id)
-                      && activity.connecting_accepted && activity.connecting_to_accepted))).map((activity) =>
-                        <tr key={activity.connections_id}>
-                          <td>{activity.connector_first_name} {activity.connector_last_name}</td>
-                          <td>connected you with</td>
-                          {activity.connecting_id === this.props.user.id ?
-                            <td>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</td>
-                            :
-                            <td>{activity.connecting_first_name} {activity.connecting_last_name}</td>
-                          }
-                        </tr>
-                      )}
-                </tbody>
-              </table>
-              : ''
-            }
-          </div>
-          <div className="people-you-connected">
-            <h3>People You Connected</h3>
-            {(this.props.userActivity.length > 0) ?
-              <table>
-                <tbody>
-                  {(this.props.userActivity.filter(activity => activity.connector_id === this.props.user.id)).map((activity) =>
-                    <tr key={activity.connections_id}>
-                      <td>{activity.connecting_first_name} {activity.connecting_last_name}</td>
-                      <td>connected with</td>
-                      <td>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-              : ''
-            }
-          </div>
-          <pre>
-            {JSON.stringify(this.props.userActivity, null, 2)}
-            {/* {JSON.stringify((this.props.userActivity.filter(activity => activity.connector_id === this.props.user.id)), null, 2)} */}
-          </pre>
+                    </div>
+                  </div>
+                )
+            : ''
+          }
         </div>
+        <div className="activity-people-who-connected-you-container">
+          <h3>People Who Connected You</h3>
+          {(this.props.userActivity.length > 0) ?
+            <table>
+              <tbody>
+                {(this.props.userActivity.filter(activity =>
+                  ((activity.connecting_id === this.props.user.id || activity.connecting_to_id === this.props.user.id)
+                    && activity.connecting_accepted && activity.connecting_to_accepted))).map((activity) =>
+                      <tr key={activity.connections_id}>
+                        <td>{activity.connector_first_name} {activity.connector_last_name}</td>
+                        <td>connected you with</td>
+                        {activity.connecting_id === this.props.user.id ?
+                          <td>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</td>
+                          :
+                          <td>{activity.connecting_first_name} {activity.connecting_last_name}</td>
+                        }
+                      </tr>
+                    )}
+              </tbody>
+            </table>
+            : ''
+          }
+        </div>
+        <div className="activity-people-you-connected-container">
+          <h3>People You Connected</h3>
+          {(this.props.userActivity.length > 0) ?
+            <table>
+              <tbody>
+                {(this.props.userActivity.filter(activity => activity.connector_id === this.props.user.id)).map((activity) =>
+                  <tr key={activity.connections_id}>
+                    <td>{activity.connecting_first_name} {activity.connecting_last_name}</td>
+                    <td>connected with</td>
+                    <td>{activity.connecting_to_first_name} {activity.connecting_to_last_name}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+            : ''
+          }
+        </div>
+        <pre>
+          {JSON.stringify(this.props.userActivity, null, 2)}
+          {/* {JSON.stringify((this.props.userActivity.filter(activity => activity.connector_id === this.props.user.id)), null, 2)} */}
+        </pre>
+
       </div>
     )
   }
