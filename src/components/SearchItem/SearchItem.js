b0a8d120073027b1d249fcc22c23c2174e38fe5a
Grant Krieger <fgkrieger13@gmail.com>
2020-01-06 14:49:08 -0600

    touches up styling for search page

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -6,6 +6,13 @@ class SearchItem extends Component {
 
       <table>
         <tbody>
+          {this.props.results.length > 0 && 
+            <tr>
+            <td></td>
+            <td></td>
+            <td ><h2 className="search-head-row">Services</h2></td>
+            <td><h2 className="search-head-row">Skills</h2></td>
+            </tr>}
           {this.props.results.map(result =>
             <tr>
               <td><img className="search-avatar" src={result.avatar} /></td>
