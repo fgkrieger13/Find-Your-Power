ec1bfdd34892bb19ea3548c2218b45ac5d8517ac
Victoria Ukatu <victoria.ukatu@gmail.com>
2020-01-07 10:23:03 -0600

    Add code comments for routes

--- server/routes/image-url.router.js
+++ server/routes/image-url.router.js
@@ -3,15 +3,9 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-    
-});
 
 /**
- * POST route to add an image to user's profile
+ * POST route to add a profile picture to user's profile
  */
 router.post('/', rejectUnauthenticated, (req, res) => {
     console.log(req.body.imageUrl);
