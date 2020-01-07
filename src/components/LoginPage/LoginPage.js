ab54d6e62b15d654d24f96fb10e1b9d1543570ab
Brooks Geenen <brgeen@gmail.com>
2020-01-07 11:33:58 -0600

    style login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -31,72 +31,63 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div>
-        {this.props.errors.loginMessage && (
-          <h2
-            className="alert"
-            role="alert"
-          >
-            {this.props.errors.loginMessage}
-          </h2>
-        )}
-        <form onSubmit={this.login}>
-          <h1>Login</h1>
-          <div>
+      <div className="login-container">
+        <div>
+          {this.props.errors.loginMessage && (
+            <h2
+              className="alert"
+              role="alert"
+            >
+              {this.props.errors.loginMessage}
+            </h2>
+          )}
+          <form onSubmit={this.login}>
+            <h1>Login</h1>
             <label htmlFor="username">
-              Email:
               <input
                 type="text"
                 name="username"
+                placeholder="EMAIL"
                 value={this.state.username}
                 onChange={this.handleInputChangeFor('username')}
               />
             </label>
-          </div>
-          <div>
             <label htmlFor="password">
-              Password:
               <input
                 type="password"
                 name="password"
+                placeholder="PASSWORD"
                 value={this.state.password}
                 onChange={this.handleInputChangeFor('password')}
               />
             </label>
-          </div>
-          <div>
-            <input
-              className="log-in"
-              type="submit"
-              name="submit"
-              value="Log In"
-            />
-          </div>
-        </form>
-        <center>
-          <button
+          </form>
+        </div>
+        <div className="login-buttons-container">
+          <input
+            className="log-in"
+            type="submit"
+            name="submit"
+            value="Log In"
+          />
+          <input
             type="button"
-            className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_REGISTER_MODE'})}}
-          >
-            Register
-          </button>
-          <button
+            className="register"
+            value="Register"
+            onClick={() => { this.props.dispatch({ type: 'SET_TO_REGISTER_MODE' }) }}
+          />
+          <input
             type="button"
-            className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_FORGOT_PASSWORD'})}}
-          >
-            Forgot Password
-          </button>
-        </center>
+            className="forgot-password"
+            value="Forgot Password"
+            onClick={() => { this.props.dispatch({ type: 'SET_TO_FORGOT_PASSWORD' }) }}
+          />
+        </div>
       </div>
     );
   }
 }
 
-// Instead of taking everything from state, we just want the error messages.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({errors}) => ({ errors });
 const mapStateToProps = state => ({
   errors: state.errors,
 });
