55ec33db9592e94b59a2623471b023f77e677173
Grant Krieger <fgkrieger13@gmail.com>
2020-01-07 16:45:11 -0600

    sets up conditional rendering and checkbox

--- src/components/SearchInitiateConnection/SearchInitiateConnection.js
+++ src/components/SearchInitiateConnection/SearchInitiateConnection.js
@@ -4,21 +4,36 @@ import { connect } from 'react-redux';
 // Show name and avatar of search result
 // On click of username user will be directed to that person's profile
 class SearchInitiateConnection extends Component {
+
+  state = {
+    clicked: '',
+  }
   render() {
     return (
       <table>
         <tbody>
           {this.props.results.map(result =>
-            <tr>
+          <tr>
               <td><img className="search-avatar2" src={result.avatar} /></td>
               <td>
-                <h3 onClick={() => {this.props.dispatch({type: 'SEARCH_NAME_CLICKED', payload: result.id})}}>
+                <h3 onClick={() => {this.props.dispatch({type: 'SEARCH_NAME_CLICKED', payload: result.id})
+              this.setState({clicked: result.id})}}>
                   {result.first_name} {result.last_name}
                 </h3>
               </td>
-            </tr>
+              {this.state.clicked == result.id ?
+              <td><button className='clicked' onClick={() => {this.setState({clicked: result.id}) 
+              }}>X</button></td>
+              :
+              <td><button className='clicked' onClick={() => {
+                this.setState({clicked: result.id})
+                }
+            }></button></td>
+               }
+            </tr> 
           )}
         </tbody>
+        <pre>{JSON.stringify(this.state.clicked, null, 2)}</pre>
       </table>
     )
   }
